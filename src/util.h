55a4790456420e63b59ea79e77cc63d752534c2d
Richard Wohlbold <richard@rgwohlbold.de>
2025-04-21 09:11:13 +0200

    Fix tests, heap overflows and memory leaks

--- src/util.h
+++ src/util.h
@@ -4,6 +4,7 @@
 
 #include "implicant.h"
 
-implicant allocate_minterm_array(int num_bits);
+implicant allocate_minterm_array(int num_bits, int num_minterms);
 bool *allocate_boolean_array(int num_elements);
 void flush_cache(bool *array, int num_elements);
+int calculate_num_implicants(int num_bits);
