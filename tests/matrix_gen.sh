717c9248e238f1b307a9dfda5c8d18acc16d70d3
amedic <amedic@student.ethz.ch>
2025-05-03 18:59:44 +0200

    Add generator and some generated tests

---
+++ tests/matrix_gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+testp="gen_tests"
+mkdir -p $testp
+SEED=10433
+percentages=(10 20 40 50 70 90)
+dir=
+for pct in "${percentages[@]}"; do
+    for n in {5..11}; do
+        [ -f $testp/rnd-$n-$pct.txt ] && continue
+        python tester.py generate $n $pct -s $SEED > $testp/rnd-$n-$pct.txt
+        python tester.py verify $testp/rnd-$n-$pct.txt
+    done
+done
