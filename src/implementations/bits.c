29e4734d0ededaa95d42f1db2742ffad0234d3e2
Richard Wohlbold <richard@rgwohlbold.de>
2025-05-04 13:56:48 +0200

    Make bits implementation faster, but still slower than baseline

--- src/implementations/bits.c
+++ src/implementations/bits.c
@@ -18,24 +18,46 @@ static void merge_implicants_bits(bitmap implicants, bitmap merged, size_t input
         for (int block = 0; block < num_blocks; block++) {
             int idx1 = input_index + 2 * block * block_len;
             int idx2 = input_index + 2 * block * block_len + block_len;
-            for (int k = 0; k < block_len; k++) {
-                bool impl1 = BITMAP_CHECK(implicants, idx1);
-                bool impl2 = BITMAP_CHECK(implicants, idx2);
-                bool merged1 = BITMAP_CHECK(merged, idx1);
-                bool merged2 = BITMAP_CHECK(merged, idx2);
-                bool res = impl1 && impl2;
-                bool merged1_ = merged1 || res;
-                bool merged2_ = merged2 || res;
 
-                BITMAP_SET(merged, idx1, merged1_);
-                BITMAP_SET(merged, idx2, merged2_);
+            if (block_len < 8) {
+                for (int k = 0; k < block_len; k++) {
+                    bool impl1 = BITMAP_CHECK(implicants, idx1);
+                    bool impl2 = BITMAP_CHECK(implicants, idx2);
+                    bool merged1 = BITMAP_CHECK(merged, idx1);
+                    bool merged2 = BITMAP_CHECK(merged, idx2);
+                    bool res = impl1 && impl2;
+                    bool merged1_ = merged1 || res;
+                    bool merged2_ = merged2 || res;
 
-                if (i >= first_difference) {
-                    BITMAP_SET(implicants, o_idx, res);
-                    o_idx++;
+                    BITMAP_SET(merged, idx1, merged1_);
+                    BITMAP_SET(merged, idx2, merged2_);
+
+                    if (i >= first_difference) {
+                        BITMAP_SET(implicants, o_idx, res);
+                        o_idx++;
+                    }
+                    idx1++;
+                    idx2++;
                 }
-                idx1++;
-                idx2++;
+            } else {
+                for (int k = 0; k < block_len; k += 8) {
+                    uint8_t impl1 = implicants.bits[idx1 / 8];
+                    uint8_t impl2 = implicants.bits[idx2 / 8];
+                    uint8_t merged1 = merged.bits[idx1 / 8];
+                    uint8_t merged2 = merged.bits[idx2 / 8];
+                    uint8_t res = impl1 & impl2;
+                    uint8_t merged1_ = merged1 | res;
+                    uint8_t merged2_ = merged2 | res;
+
+                    merged.bits[idx1 / 8] = merged1_;
+                    merged.bits[idx2 / 8] = merged2_;
+                    if (i >= first_difference) {
+                        implicants.bits[o_idx / 8] = res;
+                        o_idx += 8;
+                    }
+                    idx1 += 8;
+                    idx2 += 8;
+                }
             }
         }
     }
@@ -74,8 +96,13 @@ prime_implicant_result prime_implicants_bits(int num_bits, int num_trues, int *t
         num_ops += 3 * iterations * remaining_bits * (1 << (remaining_bits - 1));
 #endif
     }
-    // Step 2: Scan for unmerged implicants
-    for (int i = 0; i < num_implicants; i++) {
+    for (int i = 0; i < num_implicants / 64; i++) {
+        uint64_t implicant_true = ((uint64_t*)implicants.bits)[i];
+        uint64_t merged_true = ((uint64_t*)merged.bits)[i];
+        uint64_t prime_true = implicant_true & ~merged_true;
+        ((uint64_t*)primes.bits)[i] = prime_true;
+    }
+    for (int i = num_implicants - (num_implicants % 64); i < num_implicants; i++) {
         if (BITMAP_CHECK(implicants, i) && !BITMAP_CHECK(merged, i)) {
             BITMAP_SET_TRUE(primes, i);
         }
