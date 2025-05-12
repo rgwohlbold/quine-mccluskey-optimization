78a5be10683610dfdf9ba66cd17713f02e086e87
Richard Wohlbold <richard@rgwohlbold.de>
2025-05-12 11:20:12 +0200

    Add CPU model, compiler version and flags to executable and plots

--- measure.sh
+++ measure.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
+set -e
 
-make
-
-implementations="$(./prime_implicants implementations)"
 min_bits=1
-max_bits=18
-num_measurements=1
+max_bits=20
+num_measurements=10
+compilers="/usr/bin/gcc /usr/bin/clang"
 measurements_file="measurements.csv"
 
-
 [ -e "$measurements_file" ] && rm "$measurements_file"
-echo "implementation,bits,cycles,ops" > "$measurements_file"
-for implementation in $implementations; do
-    for k in $(seq 1 "$num_measurements"); do
-        for i in $(seq $min_bits $max_bits); do
-            ./prime_implicants measure "$implementation" "$i"
+echo "compiler_version,compiler_flags,cpu_model,implementation,bits,cycles,ops" > "$measurements_file"
+
+for compiler in $compilers; do
+    # since we change the compiler, we have to set the other options in another cmake run
+    cmake . -D CMAKE_C_COMPILER="$compiler"
+    cmake . -D LOG_LEVEL=2
+    make clean
+    make -j $(nproc)
+    implementations="$(./prime_implicants implementations)"
+    for implementation in $implementations; do
+        if [ "$implementation" = "baseline" ]; then
+            continue
+        fi
+        for k in $(seq 1 "$num_measurements"); do
+            for i in $(seq $min_bits $max_bits); do
+                ./prime_implicants measure "$implementation" "$i"
+            done
         done
     done
 done
