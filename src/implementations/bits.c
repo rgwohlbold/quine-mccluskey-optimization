7254ad4af1184f4a62d07170fe510e91646863c4
Richard Wohlbold <richard@rgwohlbold.de>
2025-05-27 15:05:08 +0200

    Fix one compilation error in bits.c

--- src/implementations/bits.c
+++ src/implementations/bits.c
@@ -4,12 +4,12 @@
 #include "common.h"
 #include "../util.h"
 #include "../vtune.h"
+#include "../my_signpost.h"
 #ifdef __x86_64__
 #include "../tsc_x86.h"
 #endif
 #ifdef __aarch64__
 #include "../vct_arm.h"
-#include "../my_signpost.h"
 #endif
 #include "../debug.h"
 
