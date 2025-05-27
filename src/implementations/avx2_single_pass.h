b19b89d199f7ec316fb863eef26192baa6d39ae8
Richard Wohlbold <richard@rgwohlbold.de>
2025-05-27 18:27:09 +0200

    Remove declaration from avx2_single_pass

--- src/implementations/avx2_single_pass.h
+++ src/implementations/avx2_single_pass.h
@@ -4,5 +4,4 @@
 #include "../implicant.h"
 
 prime_implicant_result prime_implicants_avx2_single_pass(int num_bits, int num_trues, int *trues);
-void merge_implicants_avx2_single_pass(bitmap implicants, bitmap merged, size_t input_index, size_t output_index, int num_bits, int first_difference);
 #endif
