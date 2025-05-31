7a538a3f6ea6e856f1ac984ed8db221013ccc092
AkileePikilee <aleksamicanovic@proton.me>
2025-05-31 14:39:11 +0200

    for loop separation + inline single register merge steps instead of ifs

---
+++ src/implementations/avx2_sp_aleksa.c
@@ -0,0 +1,9 @@
+#ifdef __AVX2__
+#include "merge/avx2_sp_aleksa.h"
+
+#define IMPLEMENTATION_FUNCTION prime_implicants_avx2_sp_aleksa
+#define MERGE_FUNCTION merge_avx2_sp_aleksa
+
+#include "algorithms/sp.h"
+
+#endif
--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@ void print_usage(char *argv[]) { LOG_INFO("usage: %s [test|measure|help|implemen
 void print_test_usage(char *argv[]) {
     LOG_INFO("usage: %s test <testfile1> <testfile2> ...", argv[0]);
 }
+void print_test_single_usage(char *argv[]) {
+    LOG_INFO("usage: %s test_single <implementation> <testfile1> <testfile2> ...", argv[0]);
+}
 void print_measure_usage(char *argv[]) { LOG_INFO("usage: %s measure <implementation> <num_bits>", argv[0]); }
 
 int parse_int(const char *s) {
@@ -31,6 +34,13 @@ int main(int argc, char *argv[]) {
             return 0;
         }
         test_implementations(&argv[2], argc - 2);  // Pass the test files to the test_implementations function
+    } else if (strcmp(argv[1], "test_single") == 0) {
+        //bitmap_test();
+        if (argc <= 3) {
+            print_test_single_usage(argv);
+            return 0;
+        }
+        test_implementation_single(argv[2], &argv[3], argc - 3);  // Pass the test files to the test_implementation function
     } else if (strcmp(argv[1], "implementations") == 0) {
         print_implementations();
     } else if (strcmp(argv[1], "merge_implementations") == 0) {
