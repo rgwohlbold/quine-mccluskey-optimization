535f2f2c488777e2baab3a11f8c1b360c3e6e832
Richard Wohlbold <richard@rgwohlbold.de>
2025-06-02 10:18:35 +0200

    Try some things

---
+++ src/implementations/avx2_sp_richard.c
@@ -0,0 +1,9 @@
+#ifdef __AVX2__
+#include "merge/avx2_sp_richard.h"
+
+#define IMPLEMENTATION_FUNCTION prime_implicants_avx2_sp_richard
+#define MERGE_FUNCTION merge_avx2_sp_richard
+
+#include "algorithms/sp.h"
+
+#endif
