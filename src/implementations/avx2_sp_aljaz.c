cce48e489caaa3224495c1b11500acf98b9b9e14
Richard Wohlbold <richard@rgwohlbold.de>
2025-06-04 17:13:00 +0200

    Rename "aljaz" -> "unroll" and remove shuffles

--- src/implementations/avx2_sp_aljaz.c
+++
@@ -1,9 +0,0 @@
-#ifdef __AVX2__
-#include "merge/avx2_sp_aljaz.h"
-
-#define IMPLEMENTATION_FUNCTION prime_implicants_avx2_sp_aljaz
-#define MERGE_FUNCTION merge_avx2_sp_aljaz
-
-#include "algorithms/sp.h"
-
-#endif
