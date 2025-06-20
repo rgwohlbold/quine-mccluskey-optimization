31847a64bc2aefda258a711bbd31572c2f9f4557
AkileePikilee <aleksamicanovic@proton.me>
2025-06-20 09:50:06 +0200

    changes to the measure script

--- measure_multiple_impl.sh
+++ measure_multiple_impl.sh
@@ -7,8 +7,8 @@ implementations=(
 # bits
 # pext
 # pext_sp
-# pext_sp_unroll
-# pext_sp_unroll_ilp
+pext_sp_unroll
+pext_sp_unroll_ilp
 # pext_sp_block
 # pext_sp_block2
 # pext_sp_block4
@@ -18,14 +18,14 @@ implementations=(
 # pext_sp_load_block4
 # pext_sp_load_block8
 # hellman
-avx2_sp_load_block2
-avx2_sp_load_block4
-avx2_sp_load_block8
-avx2_sp_load_block16
-avx512_sp_load_block2
-avx512_sp_load_block4
-avx512_sp_load_block8
-avx512_sp_load_block16
+# avx2_sp_load_block2
+# avx2_sp_load_block4
+# avx2_sp_load_block8
+# avx2_sp_load_block16
+# avx512_sp_load_block2
+# avx512_sp_load_block4
+# avx512_sp_load_block8
+# avx512_sp_load_block16
 )
 
 # Clean up any previously generated plots and data
